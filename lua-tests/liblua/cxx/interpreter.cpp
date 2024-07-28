5281465ac1b40ff407ed3f38897695f862a0d806
lyrahgames <lyrahgames@mailbox.org>
2024-07-29 01:51:43 +0200

    Tweak C++ test

--- lua-tests/liblua/cxx/interpreter.cpp
+++ lua-tests/liblua/cxx/interpreter.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 // Complete C++ Lua API
 //
@@ -8,12 +9,15 @@ using namespace std;
 
 int main(int argc, char **argv) {
   // Create new Lua state to run scripts in.
+  // Use `unique_ptr` to make it RAII capable.
   //
-  lua_State *L = luaL_newstate();
-  if (L == NULL) {
+  auto state =
+      unique_ptr<lua_State, decltype(&lua_close)>(luaL_newstate(), &lua_close);
+  if (!state) {
     cerr << "ERROR: Failed to create Lua state." << endl;
     return -1;
   }
+  auto L = state.get();
 
   // Open all available standard Lua libraries.
   //
@@ -22,20 +26,18 @@ int main(int argc, char **argv) {
   // Execute string to enable basic testing
   // for standard Lua test scripts.
   //
-  auto result = luaL_dostring(L, "_U=true");
+  if (luaL_dostring(L, "_U=true")) {
+    cerr << "ERROR: Failed to set test variable." << endl;
+    return -1;
+  }
 
   // Execute all Lua scripts given as command-line arguments.
   //
   for (int i = 1; i < argc; ++i) {
     auto error = luaL_dofile(L, argv[i]);
-    if (error)
-      cerr << "ERROR: Failed to run script '" << argv[i] << "'." << endl;
-    result += error;
+    if (!error)
+      continue;
+    cerr << "ERROR: Failed to run script '" << argv[i] << "'." << endl;
+    return -1;
   }
-
-  // Clean up Lua state at the end of the program.
-  //
-  lua_close(L);
-
-  return result;
 }
